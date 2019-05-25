9f438ef931404269acccf6938a3a9c38f3a896b9
Swati Rout <swatirout@MacBook-Pro.local>
2019-05-25 22:20:24 +0530

    "swati branch 2"

--- xcodegithub/ViewController.swift
+++ xcodegithub/ViewController.swift
@@ -13,6 +13,8 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         print("swati fbfggf");
+        print("swati branch 2");
+
         // Do any additional setup after loading the view, typically from a nib.
     }
 
